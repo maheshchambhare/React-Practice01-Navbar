2da849a5955e5512a6fa6a3507d150679e989bc5
mahesh <chambharem7@gmail.com>
2021-01-25 14:11:07 +0530

    first-react-project

---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./header.css";
+function Header() {
+  return (
+    <div>
+      <ul>
+        <a href="#">
+          <li>Menu</li>
+        </a>
+        <a href="#">
+          <li>About</li>
+        </a>
+        <a href="#">
+          <li>Services</li>
+        </a>
+        <a href="#">
+          <li>Feedback</li>
+        </a>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
